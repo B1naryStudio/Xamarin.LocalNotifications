07c540682c67fbb1978d62b36e5b735575df5e5a
sbondini <tobondini@gmail.com>
2015-01-04 20:02:36 +0200

    add basic implementation of local notifications for WindowsPhone

---
+++ LocalNotifications/LocalNotifications.Plugin.WindowsPhone81/LocalNotifier.cs
@@ -0,0 +1,27 @@
+using Windows.Data.Xml.Dom;
+using Windows.UI.Notifications;
+using LocalNotifications.Plugin.Abstractions;
+using System;
+
+
+namespace LocalNotifications.Plugin
+{
+  /// <summary>
+  /// Implementation for LocalNotifications
+  /// </summary>
+  public class LocalNotifier : ILocalNotifier
+  {
+      public void Notify(LocalNotification notification)
+      {
+          var tileXml = TileUpdateManager.GetTemplateContent(TileTemplateType.TileSquare150x150Text02);
+
+          var tileTitle = tileXml.GetElementsByTagName("text");
+          ((XmlElement)tileTitle[0]).InnerText = notification.Title;
+          ((XmlElement)tileTitle[1]).InnerText = notification.Text;
+
+          var tileNotification = new TileNotification(tileXml);
+          TileUpdateManager.CreateTileUpdaterForApplication().Update(tileNotification);
+      }
+
+  }
+}
